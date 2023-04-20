50bf9e8aaf594b833924842154d4f1328e38aeb3
tnedr <tamasnagy1976@gmail.com>
2023-04-20 10:50:14 +0200

    click automation

--- click_automate.py
+++ click_automate.py
@@ -2,6 +2,9 @@ import pyautogui
 import time
 import keyboard
 import pygetwindow as gw
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%H:%M:%S')
 
 def is_mouse_moving():
     prev_x, prev_y = pyautogui.position()
@@ -10,31 +13,24 @@ def is_mouse_moving():
     return prev_x != current_x or prev_y != current_y
 
 def click_automate(x_coord, y_coord, click_interval=60, inactivity_threshold=5):
-
     last_click_time = time.time()
+    last_activity_time = time.time()
 
     while True:
         current_time = time.time()
-        # print('continuous current_time:', current_time)
+
+        # Continuously check for user activity and update the last_activity_time
+        while is_mouse_moving() or keyboard.is_pressed('*'):
+            last_activity_time = time.time()
+            time.sleep(0.1)
+        logging.info("Inactivity detected")
 
         # If the time since the last click is greater than the click interval
         if current_time - last_click_time > click_interval:
-
-            print('I would like to click now:', current_time)
-
-            # we can have in different stages I am active or inactive
-            # inactive means that there was no activity for a certain period of time
-
-            # Wait for inactivity in the personal window
-            while is_mouse_moving() or keyboard.is_pressed('*'):
-                print('you are active:', current_time)
-                time.sleep(0.1)
-
-            # I think we have to set last active here
-
-            # Perform the click after a period of inactivity
-            if current_time - last_click_time > click_interval + inactivity_threshold:
-                print('you were inactive, so clicking:', current_time)
+            logging.info("It is time to click")
+            # If the time since the last activity is greater than the inactivity threshold
+            if current_time - last_activity_time > inactivity_threshold:
+                logging.info("Clicking")
 
                 # Get the title of the active window
                 original_window = gw.getActiveWindow()
@@ -59,7 +55,7 @@ def click_automate(x_coord, y_coord, click_interval=60, inactivity_threshold=5):
 def get_current_position():
     while True:
         current_x, current_y = pyautogui.position()
-        print("Current position:", current_x, current_y)
+        logging.info("Current position: %s, %s", current_x, current_y)
         time.sleep(1)
 
 click_automate(x_coord=1988, y_coord=365, click_interval=5, inactivity_threshold=5)
