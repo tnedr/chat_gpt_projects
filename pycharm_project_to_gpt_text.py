978397dfafdc2b32def3bcd00f46a2c43a461873
tnedr <tamasnagy1976@gmail.com>
2023-04-23 08:13:22 +0200

    pycharm_project_to_gpt_text.py

--- pycharm_project_to_gpt_text.py
+++ pycharm_project_to_gpt_text.py
@@ -1,3 +1,4 @@
+
 import os
 
 def generate_project_structure(root_dir, max_token_length, output_dir, max_tokens_per_file=10**6):
@@ -8,11 +9,15 @@ def generate_project_structure(root_dir, max_token_length, output_dir, max_token
                 path = os.path.join(root, file)
                 with open(path, 'r') as f:
                     content = f.read()
+                    if len(content.split()) > max_token_length:
+                        raise ValueError("Module '{}' has too many tokens ({}) to fit in one text file.".format(path, len(content.split())))
                     prefix = "ok this is the content of {} module:\n".format(file)
                     module_files.append((path, prefix + content))
 
+    basename = os.path.basename(os.path.normpath(root_dir))
+
     # Generate a text file containing the directory structure
-    structure_file_path = os.path.join(output_dir, 'project_structure.txt')
+    structure_file_path = os.path.join(output_dir, '{}_structure.txt'.format(basename))
     with open(structure_file_path, 'w') as f:
         f.write("Project structure:\n")
         for root, dirs, files in os.walk(root_dir):
@@ -28,27 +33,63 @@ def generate_project_structure(root_dir, max_token_length, output_dir, max_token
                 if file.endswith('.py'):
                     f.write('{}{}\n'.format(subindent, file))
 
-    # Generate separate text files for each module
-    for path, content in module_files:
-        module_name = os.path.basename(path)[:-3]  # remove the '.py' extension
-        module_file_path = os.path.join(output_dir, module_name + '.txt')
-        if len(content.split()) <= max_tokens_per_file:
-            with open(module_file_path, 'w') as f:
-                f.write(content)
-        else:
-            for i in range(0, len(content), max_tokens_per_file):
-                module_content = content[i:i+max_tokens_per_file]
-                if len(module_content.split()) > max_tokens_per_file:
-                    raise ValueError("Module '{}' has too many tokens ({}) to fit in one text file.".format(path, len(module_content.split())))
-                with open(module_file_path, 'w') as f:
-                    f.write(module_content)
+    # Generate a jumbo file containing all module content
 
-    return module_files
+    jumbo_file_path = os.path.join(output_dir, '{}_codes.txt'.format(basename))
+    with open(jumbo_file_path, 'w') as f:
+        for path, content in module_files:
+            if len(content.split()) <= max_tokens_per_file:
+                f.write(content)
+                f.write('\n') # Add a newline between modules
+            else:
+                for i in range(0, len(content), max_tokens_per_file):
+                    f.write(content[i:i+max_tokens_per_file])
+                    f.write('\n')
+
+    return jumbo_file_path
+
+
+def split_jumbo_file(jumbo_file_path, basename, output_dir, max_tokens_per_file):
+    current_file_tokens = 0
+    current_file_modules = []
+    current_file_num = 1
+
+    with open(jumbo_file_path, 'r') as jumbo_file:
+        for line in jumbo_file:
+            line_tokens = len(line.split())
+            if current_file_tokens + line_tokens <= max_tokens_per_file:
+                print(current_file_tokens + line_tokens)
+                current_file_tokens += line_tokens
+                current_file_modules.append(line.strip())
+            else:
+                write_split_file(current_file_modules, basename, output_dir, current_file_num)
+                current_file_tokens = line_tokens
+                current_file_modules = [line.strip()]
+                current_file_num += 1
+
+        # Write the last file
+        write_split_file(current_file_modules, basename, output_dir, current_file_num)
+
+def write_split_file(modules, basename, output_dir, file_num):
+    file_path = os.path.join(output_dir, '{}_{}.txt'.format(basename, file_num))
+    with open(file_path, 'w') as f:
+        for module in modules:
+            f.write(module)
+            f.write('\n')
 
 
 root_dir = 'C:/Users/Tamas/PycharmProjects/anylog_solution/'
 max_token_length = 4096
 output_dir = './output/project_to_text'
+
 print("Generating text files...")
-generate_project_structure(root_dir, max_token_length, output_dir)
+max_tokens_per_file = 4096
+
+# Generate jumbo file
+jumbo_file_path = generate_project_structure(root_dir, max_token_length, output_dir, max_tokens_per_file)
+
+# Split jumbo file into smaller files
+basename = os.path.basename(os.path.normpath(root_dir))
+split_jumbo_file(jumbo_file_path, basename, output_dir, max_tokens_per_file)
+
 print("Done.")
