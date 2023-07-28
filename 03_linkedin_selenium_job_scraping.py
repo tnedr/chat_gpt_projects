869aefa34007d56bb7964e1b4d93d1fb7150fd46
tnedr <tamasnagy1976@gmail.com>
2023-07-28 12:42:31 +0200

    first version

--- 03_linkedin_selenium_job_scraping.py
+++ 03_linkedin_selenium_job_scraping.py
@@ -38,12 +38,8 @@ element = wd.find_element(By.CSS_SELECTOR, 'h1>span')
 inner_text = element.get_attribute('innerText')
 no_of_jobs = extract_number(inner_text)
 
-# no_of_jobs = int(wd.find_element_by_css_selector(‘h1>span’).get_attribute(‘innerText’))
-print(no_of_jobs)
-
 # browse all the jobs
-
-def browse_all_jobs(wd, no_of_jobs, sleep_time=5):
+def browse_down_all_jobs(wd, no_of_jobs, sleep_time=5):
     # measure of time to browse all jobs
     start_time = time.time()  # Start time measurement
 
@@ -66,25 +62,27 @@ def browse_all_jobs(wd, no_of_jobs, sleep_time=5):
     execution_time = time.time() - start_time  # Calculate execution time
     logging.info('Execution time: %s seconds', execution_time)
 
-browse_all_jobs(wd, no_of_jobs, 3)
+browse_down_all_jobs(wd, no_of_jobs, 3)
 
-# job_lists = wd.find_element_by_class_name(‘jobs-search__results-list’)
 job_lists = wd.find_element(By.CLASS_NAME, 'jobs-search__results-list')
 jobs = job_lists.find_elements(By.TAG_NAME, 'li')
-# jobs = jobs_lists.find_elements_by_tag_name(‘li’) # return a list
-
-print(len(jobs))
 
 
+# get information without clicking to the job
 job_id = []
 job_title = []
 company_name = []
 location = []
 date = []
-job_link = []
+job_links = []
 for job in jobs:
-    job_id0 = job.get_attribute('data - id')
-    job_id.append(job_id0)
+
+    element = job.find_element(By.CSS_SELECTOR,'div[data-entity-urn]')
+    entity_urn = element.get_attribute('data-entity-urn')
+    job_id_temp = entity_urn.split(':')[-1]
+
+    # job_id0 = job.get_attribute('data - id')
+    job_id.append(job_id_temp)
 
     job_title0 = job.find_element(By.CSS_SELECTOR, 'h3').get_attribute('innerText')
     job_title.append(job_title0)
@@ -99,21 +97,22 @@ for job in jobs:
     date.append(date0)
 
     job_link0 = job.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
-    job_link.append(job_link0)
+    job_links.append(job_link0)
 
 
-jd = []
-seniority = []
-emp_type = []
-job_func = []
-industries = []
-for item in range(len(jobs)):
-
+job_descriptions = []
+job_characteristics = []
+# for item in range(len(jobs)):
+for i, job_link in enumerate(job_links):
+    print(i)
+    print(job_link)
     # clicking to given job
-    job = wd.find_element(By.CLASS_NAME, 'jobs-search__results-list').find_element(By.XPATH, f'li[{item + 1}]')
-    job_click_path = f'div / a'
-    job_href = job.find_element(By.XPATH, job_click_path).get_attribute('href')
-    wd.get(job_href)
+    # job = wd.find_element(By.CLASS_NAME, 'jobs-search__results-list').find_element(By.XPATH, f'li[{item + 1}]')
+    # job_click_path = f'div / a'
+    # job_href = job.find_element(By.XPATH, job_click_path).get_attribute('href')
+    # wd.get(job_href)
+    wd.get(job_link)
+
     time.sleep(3)
 
     # get job characteristics seniority, emp_type, job_func, industries
@@ -124,81 +123,31 @@ for item in range(len(jobs)):
         header = li.find_element(By.CLASS_NAME, 'description__job-criteria-subheader').text
         text = li.find_element(By.CLASS_NAME, 'description__job-criteria-text--criteria').text
         job_criteria[header] = text
+    job_characteristics.append(job_criteria)
 
-    print(job_criteria)
-
-
-
-    description_path = '// *[ @ id = "main-content"] / section[1] / div / div / section[1] / div / ul'
-    job_characteristics = wd.find_element(By.XPATH, description_path)
-
-    print(1)
-    # Click the 'Show more' button
-
-
-
-
-
-
-
-
-
-
-
+    # get job description
     wd.find_element(By.XPATH, '//button[contains(text(), "Show more")]').click()
     time.sleep(2)
-
     # First, get the 'section' element.
     section_element = wd.find_element(By.CSS_SELECTOR, "section.show-more-less-html")
     # Then, within that section, find the 'div' element.
     div_element = section_element.find_element(By.CSS_SELECTOR, "div.show-more-less-html__markup")
-    jd = div_element.text
-
-    seniority_path = f' / html / body / main / section / div[2] / section[2] / ul / li[1] / span'
-    seniority0 = job.find_element(By.XPATH, seniority_path).get_attribute('innerText')
-    seniority.append(seniority0)
-
-    emp_type_path = ' / html / body / main / section / div[2] / section[2] / ul / li[2] / span'
-    emp_type0 = job.find_element(By.XPATH, emp_type_path).get_attribute('innerText')
-    emp_type.append(emp_type0)
-
-    job_func_path = ' / html / body / main / section / div[2] / section[2] / ul / li[3] / span'
-    job_func_elements = job.find_elements(By.XPATH, job_func_path)
-    for element in job_func_elements:
-        job_func0.append(element.get_attribute('innerText'))
-        job_func_final = ', '.join(job_func0)
-        job_func.append(job_func_final)
-    industries_path = ' / html / body / main / section / div[2] / section[2] / ul / li[4] / span'
-    industries_elements = job.find_elements(By.XPATH, industries_path)
-    for element in industries_elements:
-        industries0.append(element.get_attribute('innerText'))
-        industries_final = ', '.join(industries0)
-        industries.append(industries_final)
-
-        # Create an instance of Chrome WebDriver
+    job_descriptions.append(div_element.text)
 
 
-# Maximize the browser window
-driver.maximize_window()
+# Create a DataFrame from the job details
+df_jobs = pd.DataFrame({
+    'Job ID': job_id,
+    'Job Title': job_title,
+    'Company Name': company_name,
+    'Location': location,
+    'Date Posted': date,
+    'Job Link': job_links,
+    'Job Description': job_descriptions,
+})
 
-# Navigate to a website
-driver.get("https://www.example.com")
+# Create a DataFrame from the job characteristics
+df_characteristics = pd.DataFrame(job_characteristics)
 
-# Find an element by its ID and click on it
-# Find an element by its ID using the find_element method
-element = driver.find_element(By.ID, "my-button")
-element.click()
-
-# Close the browser
-driver.quit()
-
-# Close the browser
-driver.quit()
-url = 'https://www.linkedin.com/jobs/search?keywords=Data%20Scientist&location=Toronto%2C%20Ontario%2C%20Canada&geoId=100025096&trk=public_jobs_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0'
-
-wd = webdriver.Chrome(executable_path="C:\Program Files\chromedriver.exe")
-
-wd.get(url)
-
-no_of_jobs = int(wd.find_element_by_css_selector('h1>span').get_attribute('innerText'))
-print(no_of_jobs)
+# Concatenate the two DataFrames along the column axis
+df = pd.concat([df_jobs, df_characteristics], axis=1)
