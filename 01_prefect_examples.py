8c509b2b082bf20de43c7cd02561f5ad4dbb8c51
tnedr <tamasnagy1976@gmail.com>
2023-08-02 11:43:14 +0200

    prefect examples

---
+++ 01_prefect_examples.py
@@ -0,0 +1,47 @@
+import time
+
+from prefect import flow, task, Flow
+# from prefect_dask import DaskTaskRunner
+from prefect.task_runners import ConcurrentTaskRunner
+from time import sleep
+import sys
+
+
+# measure of time of execution
+def measure_execution_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Execution time: {execution_time} seconds")
+        return result
+    return wrapper
+
+@task
+def count_and_sleep(x):
+    for i in range(x):
+        print(i)
+        sleep(1)
+    return 'slepping seconds of: ' + str(x)
+
+@flow
+def very_simple_flow():
+    print('ok')
+
+@flow
+def first_flow(x):
+    count_and_sleep(x)
+
+
+@measure_execution_time
+@flow(task_runner=ConcurrentTaskRunner())
+def concurrent_flow(n, x):
+    for i in range(n):
+        future = count_and_sleep.submit(x)
+        print(future)
+        print(future.result())
+        print(future.status())
+        print(type(future))
+concurrent_flow(6, 3)
+sys.exit()
